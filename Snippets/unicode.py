9fa3fe83e15802f09f815c2723ad20211777f3e9
ColinShark <nicolas@neht.xyz>
2019-09-30 23:31:46 +0200

    black formatting (again)

--- Snippets/unicode.py
+++ Snippets/unicode.py
@@ -6,8 +6,9 @@ app = Client("my_account")
 
 with app:
     for member in app.iter_chat_members(chat):
-        if member.user.is_deleted: continue
+        if member.user.is_deleted:
+            continue
         x = member.user.first_name
-        y = member.user.first_name.encode().decode('ascii', 'ignore')
+        y = member.user.first_name.encode().decode("ascii", "ignore")
         if x != y:
             print(f"{x}\t|\t{y}")
