22b827c8c691e0cdef503e57fe10ddcbce1bf38b
ColinTheShark <nicolas@neht.xyz>
2019-10-16 21:10:01 +0200

    Move msg.photo check into the if statements (flake8 compliance)

--- Snippets/photo_threshold.py
+++ Snippets/photo_threshold.py
@@ -2,16 +2,17 @@ from pyrogram import Client
 
 app = Client("my_account")
 
-threshold = 100 * 1024 # 100 kb
+threshold = 100 * 1024  # 100 kb
 below = 0
 above = 0
 exact = 0
-chat = "me" # Saved Messages, any other @username works, too.
+chat = "me"  # Saved Messages, any other @username works, too.
 
 with app:
-    for msg in app.iter_history(chat) if msg.photo:
-        if msg.photo.file_size < threshold:
+    for msg in app.iter_history(chat):
+        if msg.photo and msg.photo.file_size < threshold:
             below += 1
-        else:
+        elif msg.photo and msg.photo.file_size > threshold:
             above += 1
+
 print(f"Below: {below}\nAbove: {above}\nTotal: {above+below}")
