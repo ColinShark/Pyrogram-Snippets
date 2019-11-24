4ecf521d9346639d2d5224ebab53665bfb4bfbde
ColinTheShark <nicolas@neht.xyz>
2019-11-25 00:47:15 +0100

    Update Functions. "-" and "_" broke base64 :)

--- Snippets/resolve_invite_link.py
+++ Snippets/resolve_invite_link.py
@@ -1,4 +1,4 @@
-from base64 import b64decode
+from base64 import urlsafe_b64decode
 from struct import unpack
 
 from pyrogram import Client, Filters, Message
@@ -9,7 +9,7 @@ app = Client("my_account")
 @app.on_message(Filters.command("invite", ".") & Filters.me)
 def resolve_invite(app: Client, message: Message):
     link = message.command[1].split("/")[-1]
-    d = b64decode(link + "==")
+    d = urlsafe_b64decode(link + "==")
     message.edit_text(
         "Invite Link: `{}`\nAdmin: `{}`\nChat: `-100{}`\nHash: `{}`".format(
             link, *unpack(">iiq", d)
