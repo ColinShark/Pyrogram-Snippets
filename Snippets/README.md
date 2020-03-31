1065b788632ac7f29ccbec30a8fbb3cfac8d11b9
ColinTheShark <colin@colinshark.de>
2020-03-31 19:43:24 +0200

    Add Snippet join_date

--- Snippets/README.md
+++ Snippets/README.md
@@ -28,6 +28,7 @@ All snippets assume you have a `config.ini` with [your api id and hash set up][A
 | 20. | [Auto Scroll](auto_scroll.py) | Send `.autoscroll` to auto-read all new messages in a chat in case you don't have Telegram in focus.
 | 21. | [Filter Toggle](filter_toggle.py) | Send `.com` to disable the `.hi` handler. Helpful if you want to completely disable a handler on command.<sup>9</sup>
 | 22. | [Screenshot](screenshot.py) | Send a Screenshot Notification. Works only in Private Chats.
+| 23. | [Joined Date](join_date.py) | Get a list of all members sorted by date joined.
 
 <sup>1: Iterating through a lot of messages might take some time.<br>
 2: This includes Supergroups, as Telegram handles them the same internally.<br>
---
+++ Snippets/join_date.py
@@ -0,0 +1,38 @@
+# Get a list of all chat members and sort them by the date they joined the group
+# The Creator of the group has no join_date internally,
+# so he gets the group creation date
+
+import os
+from datetime import datetime
+
+from pyrogram import Client, Filters, Message
+
+app = Client("my_account")
+
+
+@app.on_message(Filters.command("joindate") & Filters.me)
+def join_date(app: app, message: Message):
+    members = []
+    for m in app.iter_chat_members(message.chat.id):
+        members.append(
+            (
+                m.user.first_name,
+                m.joined_date or app.get_messages(message.chat.id, 1).date,
+            )
+        )
+
+    members.sort(key=lambda member: member[1])
+
+    with open("joined_date.txt", "w", encoding="utf8") as f:
+        f.write("Join Date      First Name\n")
+        for member in members:
+            f.write(
+                str(datetime.fromtimestamp(member[1]).strftime("%y-%m-%d %H:%M"))
+                + f" {member[0]}\n"
+            )
+
+    app.send_document(message.chat.id, "joined_date.txt")
+    os.remove("joined_date.txt")
+
+
+app.run()
